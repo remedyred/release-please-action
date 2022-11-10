d9e0bc529fde78f075c7759bedd1a392de429d97
Snickbit <snickbit@users.noreply.github.com>
2022-11-10 18:09:13 -0500

    fix: don't force manifest on monorepo

--- src/common.sh
+++ src/common.sh
@@ -120,10 +120,6 @@ if ((__LOADED == 0)); then
   [[ -z "$AUTOFIX_LOCKFILE" ]] && AUTOFIX_LOCKFILE=true
   [[ -z "$MONOREPO" ]] && MONOREPO=false
 
-  if [[ -z "$RELEASE_COMMAND" ]] && [[ "$MONOREPO" == "true" ]]; then
-    RELEASE_COMMAND="manifest"
-  fi
-
   debug "DRY_RUN: $DRY_RUN"
   debug "DEBUG: $DEBUG"
   debug "NO_BAIL: $NO_BAIL"
