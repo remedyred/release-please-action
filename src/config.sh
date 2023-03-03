3cdc4cd25e4c8596853d05127e7289955aa39439
Snickbit <snickbit@users.noreply.github.com>
2023-03-03 10:55:17 -0500

    feat: add option to automatically commit untracked changes

--- src/config.sh
+++ src/config.sh
@@ -98,6 +98,16 @@ for script in "${PRERELEASE_SCRIPTS_ARRAY[@]}"; do
   runScript "$script"
 done
 
+if [[ ("$AUTO_COMMIT" == "true" && "$PRERELEASE_ONLY" != "true") || "$AUTO_COMMIT_PRERELEASE" == "true" ]]; then
+  if [[ -n "$(git status --porcelain)" ]]; then
+    # commit untracked changes created during prerelease, before moving into the release script
+    git add .
+    git commit -m "chore: untracked changes from prerelease [skip ci]"
+    git push
+  fi
+fi
+
+# Outputs
 if [[ "$PRERELEASE_ONLY" != "true" ]]; then
   echo "should-release=true" >>"$GITHUB_OUTPUT"
 
